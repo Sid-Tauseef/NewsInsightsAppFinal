8b372c3955c998d8eca3506e3550404a9afecd77
Tauseeb Ahmed Siddiqui <122994540+Sid-Tauseef@users.noreply.github.com>
2025-04-23 21:28:24 +0530

    Update config.dart

--- client/lib/constants/config.dart
+++ client/lib/constants/config.dart
@@ -1,10 +1,10 @@
 // this is my config.dart
 class Config {
-  static const String userApiUrl = 'http://192.168.187.158:8000/';
+  static const String userApiUrl = 'http://"YOUR_WIFI_IP_ADDRESS:8000/';
   static const String registrationUrl = "${userApiUrl}registration";
   static const String login = '${userApiUrl}login';
-  static const String summarize = 'https://b655-34-56-196-205.ngrok-free.app/text-summarizer/summarize/';
-  static const String apiKey = '9d9b2a5b60a24e239b32560c58c00987';
+  static const String summarize = 'https://YOUR_NGROK_ENDPOINT_URL/text-summarizer/summarize/';
+  static const String apiKey = '"YOUR_API_KEY_FROM_newsapi.org"';
   static const String getUserDetails = '${userApiUrl}getUserDetails';
   static const String getLikedArticles = '${userApiUrl}getLikedArticles';
   static const String recommend = '${userApiUrl}recommend';
